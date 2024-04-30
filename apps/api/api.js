5c4a052739023e31c9071509794fab28802855f7
calangthang1406 <90557694+calangthang1406@users.noreply.github.com>
2024-04-30 18:36:46 +0700

    update: login, register api

---
+++ apps/api/api.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const api = new express.Router();
+
+const initApi = (app) => {
+    app.set("json spaces", 2);
+    app.use("/api", api);
+};
+
+api.use(bodyParser.json());
+
+api.use('/users', require('../routers/users'))
+
+module.exports = initApi;
