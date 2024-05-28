255fba67d49cbcd2057d9b5a9f1dc8dae9226ac2
calangthang1406 <90557694+carp-calangthang@users.noreply.github.com>
2024-05-29 00:16:20 +0700

    update: play music api

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const initApi = require('./apps/api/api');
 
+const cors = require('cors');
 const express = require('express');
 const bodyParser = require('body-parser');
 const dotenv = require('dotenv').config();
@@ -10,6 +11,16 @@ const app = express();
 
 connection();
 
+app.use((req, res, next) => {
+  const clientIp = req.ip;
+  const requestMethod = req.method;
+  const requestUrl = req.originalUrl;
+
+  console.log(`User: ${clientIp} have sent request: ${requestMethod} ${requestUrl}`);
+  next();
+});
+
+app.use(cors());
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(express.static('public'));
@@ -20,80 +31,3 @@ let port = 3000;
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
-
-
-// const { google } = require('googleapis');
-// const express = require('express');
-// const bodyParser = require('body-parser'); 
-// const API_KEY = 'AIzaSyDWP5fLFGegEERs7uiKhnZUdy3ZIgbUJRk'; // Thay bằng API Key của bạn
-
-// const youtube = google.youtube({
-//   version: 'v3',
-//   auth: API_KEY,
-// });
-
-// const app = express();
-// const PORT = 3000;
-
-// app.use(bodyParser.json());
-// app.use(express.static('public'));
-
-// app.get('/api/music', async (req, res) => {
-//   try {
-//     const [usResponse, vnResponse] = await Promise.all([
-//       youtube.videos.list({
-//         part: 'snippet,contentDetails',
-//         chart: 'mostPopular',
-//         regionCode: 'US', // Mã vùng Hoa Kỳ
-//         videoCategoryId: '10', // Music category ID
-//         maxResults: 5, // Số lượng video muốn lấy từ Hoa Kỳ
-//       }),
-//       youtube.videos.list({
-//         part: 'snippet,contentDetails',
-//         chart: 'mostPopular',
-//         regionCode: 'VN', // Mã vùng Việt Nam
-//         videoCategoryId: '10', // Music category ID
-//         maxResults: 5, // Số lượng video muốn lấy từ Việt Nam
-//       })
-//     ]);
-
-//     const combinedVideos = [...usResponse.data.items, ...vnResponse.data.items];
-//     res.json(combinedVideos);
-//   } catch (error) {
-//     console.error('Error fetching music videos:', error);
-//     res.status(500).send('Error fetching music videos');
-//   }
-// });
-
-
-// async function searchSongs(query) {
-//   try {
-//     const response = await youtube.search.list({
-//       part: 'snippet',
-//       q: query,
-//       type: 'video',
-//       maxResults: 50,
-//     });
-
-//     const videos = response.data.items;
-//     return videos;
-//   } catch (error) {
-//     console.error('Error searching for songs:', error);
-//     return [];
-//   }
-// }
-
-// app.post('/api/search', async (req, res) => {
-//   try {
-//     const query = req.body.query || '';
-//     const videos = await searchSongs(query);
-//     res.json(videos);
-//   } catch (error) {
-//     console.error('Error searching for songs:', error);
-//     res.status(500).json({ error: error.message });
-//   }
-// });
-
-// app.listen(PORT, () => {
-//   console.log(`Server is running on http://localhost:${PORT}`);
-// });
--- apps/controller/users/login.js
+++ apps/controller/users/login.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 const Users = require('../../models/users/users');
 
 const login = async (req, res) => {
